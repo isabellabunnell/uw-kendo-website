d0ea601f895c6560e308dfe2b8eff91f7d4427c9
LINNI2017 <cai20170607@gmail.com>
2020-12-28 01:49:58 -0800

    add awards block

--- src/views/AboutPage.js
+++ src/views/AboutPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import AwardsBlock from "../components/AwardsBlock"
 import InfoBlock from "../components/InfoBlock"
 import PageStructure from "../components/PageStructure"
 import { clubInfo } from "../data/info"
@@ -7,13 +8,18 @@ const AboutPage = () => {
    return (
       <PageStructure
          content={
-            <div style={{ marginTop: "2rem" }}>
-               <InfoBlock
-                  title={clubInfo.title}
-                  content={clubInfo.content}
-                  img={clubInfo.img}
-               />
-            </div>
+            <>
+               <div style={{ marginTop: "2rem" }}>
+                  <InfoBlock
+                     title={clubInfo.title}
+                     content={clubInfo.content}
+                     img={clubInfo.img}
+                  />
+               </div>
+               <div style={{ marginTop: "2rem" }}>
+                  <AwardsBlock />
+               </div>
+            </>
          }
       />
    )
