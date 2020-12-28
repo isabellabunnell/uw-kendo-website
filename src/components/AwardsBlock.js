2a23fce02fb95c13d13249da4aee259661e4e452
LINNI2017 <cai20170607@gmail.com>
2020-12-28 01:44:55 -0800

    add awards block

---
+++ src/components/AwardsBlock.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react"
+import { Pagination } from '@material-ui/lab'
+import EventAwards from "./EventAwards"
+
+const AwardsBlock = () => {
+   const awardsMap = require("../data/awards")
+   const yearList = Object.keys(awardsMap).map(yearString => parseInt(yearString)).reverse()
+   const [activePage, setActivePage] = useState(1)
+
+   console.log(activePage, yearList[activePage-1], awardsMap[yearList[activePage-1]])
+
+   return (
+      <>
+         <Pagination
+            size="large"
+            count={yearList.length}
+            defaultPage={1}
+            color="primary"
+            onChange={(_, page) => setActivePage(page)}
+            style={{ float: "right"}}
+         />
+         {
+            awardsMap[yearList[activePage-1]].map((event, idx) => (
+               <EventAwards key={idx} event={event} />
+            ))
+         }
+      </>
+   )
+}
+
+export default AwardsBlock
