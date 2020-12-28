a6cd09ac5cacd7f33a571972ca31e6c9f71d56d4
LINNI2017 <cai20170607@gmail.com>
2020-12-28 01:45:30 -0800

    add event awards block served smaller piece of rewards

---
+++ src/components/EventAwards.js
@@ -0,0 +1,45 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Table } from "react-bootstrap"
+
+const EventAwards = ({
+   event
+}) => {
+   console.log(event)
+   const title = Object.keys(event)[0]
+   const awards = event[title]
+
+   return (
+      <div>
+         <h3>
+            {title}
+         </h3>
+         <Table striped bordered hover>
+            <thead>
+               <tr>
+                  <th>Name</th>
+                  <th>Division</th>
+                  <th>Place</th>
+               </tr>
+            </thead>
+            <tbody>
+               {
+                  awards.map((info, idx) => (
+                     <tr key={idx}>
+                        <td>{info.name}</td>
+                        <td>{info.division}</td>
+                        <td>{info.place}</td>
+                     </tr>
+                  ))
+               }
+            </tbody>
+         </Table>
+      </div>
+   )
+}
+
+EventAwards.propTypes = {
+   event: PropTypes.object
+}
+
+export default EventAwards
