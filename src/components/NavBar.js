289773d6368b378e959f3be50c3332043bab1183
LINNI2017 <cai20170607@gmail.com>
2020-12-28 02:13:22 -0800

    add navigable links with prefix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import { Nav } from "react-bootstrap"
+import { useLocation } from "react-router-dom"
+import { WEB_URL } from "../views/App"
 
 const NavBar = () => {
+   const location = useLocation().pathname.split("/").pop()
+   const [activePage, setActivePage] = useState(location)
+   const pageList = ["about", "what-is-kendo", "join", "resources"]
+
    return (
-      <Nav defaultActiveKey="/home" as="ul" className="nav-bar-font">
+      <Nav
+         as="ul"
+         className="nav-bar-font"
+      >
          <Nav.Item as="li">
-            <Nav.Link href="/about">About Us</Nav.Link>
+            <Nav.Link href={`${WEB_URL}/about`} disabled={activePage === pageList[0]}>About Us</Nav.Link>
          </Nav.Item>
          <Nav.Item as="li">
-            <Nav.Link href="/home">What is Kendo?</Nav.Link>
+            <Nav.Link href={`${WEB_URL}`} disabled={activePage === pageList[1]}>What is Kendo?</Nav.Link>
          </Nav.Item>
          <Nav.Item as="li">
-            <Nav.Link href="/home">Join</Nav.Link>
+            <Nav.Link href={`${WEB_URL}`} disabled={activePage === pageList[2]}>Join</Nav.Link>
          </Nav.Item>
          <Nav.Item as="li">
-            <Nav.Link href="/home">Resources</Nav.Link>
+            <Nav.Link href={`${WEB_URL}`} disabled={activePage === pageList[3]}>Resources</Nav.Link>
          </Nav.Item>
       </Nav>
    )
