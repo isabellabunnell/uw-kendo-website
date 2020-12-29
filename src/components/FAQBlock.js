5b7a4693c1a1e0e217c224cf348c202e5530de1a
LINNI2017 <cai20170607@gmail.com>
2020-12-29 02:26:29 -0800

    add toggle close with up arrow

--- src/components/FAQBlock.js
+++ src/components/FAQBlock.js
@@ -1,9 +1,9 @@
 import "react-calendar/dist/Calendar.css"
-import React from "react"
+import React, { useState } from "react"
 import PageStructure from "../components/PageStructure"
 import { Accordion, Button } from "react-bootstrap"
 import { faqList } from "../data/info"
-import { BsChevronDown } from "react-icons/bs"
+import { BsChevronDown, BsChevronUp } from "react-icons/bs"
 
 const FAQBlock = () => {
    return (
@@ -12,27 +12,29 @@ const FAQBlock = () => {
             FAQ
          </h3>
          {
-            faqList.map((faq, idx) => (
-               <div className="faq-accordion" key={`faq-${idx}`}>
-                  <Accordion>
-                     <Accordion.Toggle
-                        as={Button}
-                        variant="link"
-                        eventKey="0"
-                        style={{ paddingLeft: "0", width: "100%" }} // need inline override
-                     >
-                        <div className="faq-button">
-                           <h6 style={{ fontWeight: "bold" }}>{faq.Q}</h6>
-                           <h5><BsChevronDown /></h5>
-                        </div>
-                     </Accordion.Toggle>
-                     <Accordion.Collapse eventKey="0">
-                        <p>{faq.A}</p>
-                     </Accordion.Collapse>
-                  </Accordion>
-               </div>
-               
-            ))
+            faqList.map((faq, idx) => {
+               const [open, setOpen] = useState(false)
+               return (
+                  <div className="faq-accordion" key={`faq-${idx}`}>
+                     <Accordion onClick={() => setOpen(!open)} >
+                        <Accordion.Toggle
+                           as={Button}
+                           variant="link"
+                           eventKey="0"
+                           style={{ paddingLeft: "0", width: "100%" }} // need inline override
+                        >
+                           <div className="faq-button">
+                              <h6 style={{ fontWeight: "bold" }}>{faq.Q}</h6>
+                              <h5>{ open ? <BsChevronUp /> : <BsChevronDown />}</h5>
+                           </div>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                           <p>{faq.A}</p>
+                        </Accordion.Collapse>
+                     </Accordion>
+                  </div>
+               )
+            })
          }
       </div>
    )
