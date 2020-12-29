55024c238cf75ab5eb5fa6b573c4efe626ccde17
LINNI2017 <cai20170607@gmail.com>
2020-12-28 21:40:35 -0800

    add mock info

--- src/data/info.js
+++ src/data/info.js
@@ -3,5 +3,28 @@ export const clubInfo = {
    content: `The Kendo Club at University of Washington (UW) is a sports club sponsored by the Department of Recreational Sports Programs. We're open to currently enrolled UW students and faculty/staff members that have purchased a quarterly or annual IMA Membership Card.
    \n
    We are recognized member dojo of the Pacific Northwest Kendo Federation (PNKF) and the All United States Kendo Federation (AUSKF). Members of the club are eligible to become members in good standing of both federations and are entitled to all privileges of the same.`,
-   img: "placeholder.png"
+   img: "info/placeholder.jpg"
 }
+
+export const sportInfo = [
+   {
+      title: "The Sport",
+      content: `Lorem ipsum dolor sit amet, consectetur aipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placera orci nulla pellentesque dignissim enim. Quisque sagittis purus sit amet volutpa consequa mauris nunc congue. Elementum pulvinar etiam non quam lacus suspedisse. Nullam non nisi est sit. Egestas sed tempus urna et pharetra pharetra. In dictum non consectetur a. Amet nulla facilisi morbi tempus iaculis urna. Cras fermentum odio eu feugia pretium. Lorem ipsum dolor sit amet consectetur aipiscing elit pellentesque. Fermentum leo vel orci porta non pulvinar neque. Cursus risus a ultrices mi tempus imperdiet nulla malsuaa.`,
+      img: "info/sport.jpg"
+   },
+   {
+      title: "Warm Up",
+      content: `Lorem ipsum dolor sit amet, consectetur aipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placera orci nulla pellentesque dignissim enim. Quisque sagittis purus sit amet volutpa consequa mauris nunc congue. Elementum pulvinar etiam non quam lacus suspedisse. Nullam non nisi est sit. Egestas sed tempus urna et pharetra pharetra. In dictum non consectetur a. Amet nulla facilisi morbi tempus iaculis urna. Cras fermentum odio eu feugia pretium. Lorem ipsum dolor sit amet consectetur aipiscing elit pellentesque. Fermentum leo vel orci porta non pulvinar neque. Cursus risus a ultrices mi tempus imperdiet nulla malsuaa.`,
+      img: "info/warm-up.jpg"
+   },
+   {
+      title: "Iaido",
+      content: `Lorem ipsum dolor sit amet, consectetur aipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placera orci nulla pellentesque dignissim enim. Quisque sagittis purus sit amet volutpa consequa mauris nunc congue. Elementum pulvinar etiam non quam lacus suspedisse. Nullam non nisi est sit. Egestas sed tempus urna et pharetra pharetra. In dictum non consectetur a. Amet nulla facilisi morbi tempus iaculis urna. Cras fermentum odio eu feugia pretium. Lorem ipsum dolor sit amet consectetur aipiscing elit pellentesque. Fermentum leo vel orci porta non pulvinar neque. Cursus risus a ultrices mi tempus imperdiet nulla malsuaa.`,
+      img: "info/iaido.jpg"
+   },
+   {
+      title: "Etiquette",
+      content: `Lorem ipsum dolor sit amet, consectetur aipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placera orci nulla pellentesque dignissim enim. Quisque sagittis purus sit amet volutpa consequa mauris nunc congue. Elementum pulvinar etiam non quam lacus suspedisse. Nullam non nisi est sit. Egestas sed tempus urna et pharetra pharetra. In dictum non consectetur a. Amet nulla facilisi morbi tempus iaculis urna. Cras fermentum odio eu feugia pretium. Lorem ipsum dolor sit amet consectetur aipiscing elit pellentesque. Fermentum leo vel orci porta non pulvinar neque. Cursus risus a ultrices mi tempus imperdiet nulla malsuaa.`,
+      img: "info/etiquette.jpg"
+   }
+]
