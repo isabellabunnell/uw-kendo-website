488e4d7d91c0dacafe7fa282823adede367dce4e
LINNI2017 <cai20170607@gmail.com>
2020-12-29 23:16:08 -0800

    add faq page with new shareable block

--- src/views/FAQPage.js
+++ src/views/FAQPage.js
@@ -1,13 +1,22 @@
-import "react-calendar/dist/Calendar.css"
 import React from "react"
-import FAQBlock from "../components/FAQBlock"
 import PageStructure from "../components/PageStructure"
+import ToggleBlock from "../components/ToggleBlock"
+import { faqList } from "../data/info"
 
 const FAQPage = () => {
    return (
       <PageStructure
          content={
-            <FAQBlock />
+            <div style={{ marginTop: "3rem" }}>
+               <h3 style={{ marginBottom: "1rem" }}>
+                  FAQ
+               </h3>
+               <ToggleBlock
+                  infoList={faqList}
+                  toggleName="Q"
+                  collapseName="A"
+               />
+            </div>
          }
       />
    )
