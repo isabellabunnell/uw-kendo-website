9755448f16dcd44fee236721abf054848964032d
LINNI2017 <cai20170607@gmail.com>
2020-12-30 11:27:28 -0800

    add ann. page with popover/crop image

--- src/views/AnnPage.js
+++ src/views/AnnPage.js
@@ -1,15 +1,71 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PageStructure from "../components/PageStructure"
-import ToggleBlock from "../components/ToggleBlock"
+import { annList } from "../data/info"
+import { IMAGE_PATH } from "./App"
+import { Row, Col, OverlayTrigger, Popover } from "react-bootstrap"
 
 const AnnPage = () => {
+   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+   useEffect(() => {
+      window.addEventListener("resize", () => {
+         setScreenWidth(window.innerWidth)
+      })
+   }, [])
+
+   const popover = (image) => (
+      <Popover
+         id={`popover-${image}`}
+         className="ann-popover-layout"
+         style={{ display: "block" }}
+         hidden={screenWidth <= 767}
+      >
+        <Popover.Content >
+         <img
+               src={`${IMAGE_PATH}/${image}`}
+               alt={image}
+               width="100%"
+               style={{
+                  objectFit: "cover",
+                  marginBottom: "2rem",
+                  float: "left"
+               }}
+            />
+        </Popover.Content>
+      </Popover>
+    )
+
    return (
       <PageStructure
          content={
             <div style={{ marginTop: "3rem" }}>
-               <h3 style={{ marginBottom: "1rem" }}>
+               <h3 style={{ marginBottom: "2rem" }}>
                   Announcement
                </h3>
+               <Row>
+                  {
+                     annList.map((ann, annIdx) => (
+                        <Col key={`ann-${annIdx}`} md={4} sm={6} className="ann-block-layout">
+                           <OverlayTrigger placement="right" overlay={popover(ann.img)}>
+                              <img
+                                 src={`${IMAGE_PATH}/${ann.img}`}
+                                 alt={ann.img}
+                                 width="100%"
+                                 height={screenWidth > 575 && "50%"}
+                                 style={{
+                                    objectFit: "cover",
+                                    marginBottom: "2rem",
+                                    float: "left"
+                                 }}
+                              />
+                           </OverlayTrigger>                     
+                           <h6>{ann.title}</h6>
+                           <h6 style={{ fontWeight: "lighter" }}>{ann.date}</h6>
+                           <p>{ann.content}</p>
+                        </Col>
+                     ))
+                  }
+               </Row>
             </div>
          }
       />
