0ad8e1fd61fc4dd28ea56419151ae921c6a94fab
LINNI2017 <cai20170607@gmail.com>
2020-12-31 20:36:45 -0800

    refactor join page

--- src/views/JoinPage.js
+++ src/views/JoinPage.js
@@ -1,59 +1,17 @@
-import "react-calendar/dist/Calendar.css"
-import React, { useState } from "react"
+import React from "react"
 import PageStructure from "../components/PageStructure"
-import Calendar from "react-calendar"
-import EventBlock from "../components/EventBlock"
-import { Row, Col, Button } from "react-bootstrap"
-import { getDateStr } from "../functions/utils"
-import { JOIN_MAIL_URL } from "../data/info"
+import GalleryBlock from "../components/GalleryBlock"
+import CalendarBlock from "../components/CalendarBlock"
+import JoinBlock from "../components/JoinBlock"
 
 const JoinPage = () => {
-   const [dateValue, setDateValue] = useState(new Date())
-   const calendarMap = require("../data/calendar")
-   const dateStr = getDateStr(dateValue)
-
    return (
       <PageStructure
          content={
             <div style={{ marginTop: "3rem" }}>
-               <h3>
-                  Calendar
-               </h3>
-               <p>
-                  Lorem ipsum dolor sit amet, consectetur aipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placera orci nulla pellentesque dignissim enim.   
-               </p>
-               <Row style={{ marginTop: "2rem" }}>
-                  <Col sm={6}>
-                     <Calendar
-                        className="join-calendar-layout"
-                        onChange={setDateValue}
-                        value={dateValue}
-                     />
-                  </Col>
-                  <Col sm={1} />
-                  <Col sm={5}>
-                  {
-                     dateStr in calendarMap ?
-                     calendarMap[dateStr].map((event, idx) =>
-                        <EventBlock event={event} key={`event-${idx}`} />)
-                     :
-                     <h3>Free! No event available.</h3>
-                  }
-                  </Col>
-               </Row>
-               <div style={{ marginTop: "2rem" }}>
-                  <p>
-                     We send out emails once a week with information on resources, practice schedules, etc. Joining our mailing list is the best way of getting the latest news from us. We look forward to hearing from you!
-                  </p>
-                  <Button
-                     className="join-button-font"
-                     variant="primary"
-                     style={{ backgroundColor: "#5B3080", borderColor: "#5B3080" }}
-                     onClick={() => window.location=JOIN_MAIL_URL}
-                  >
-                     Join our mailing list
-                  </Button>
-               </div>
+               <CalendarBlock />
+               <GalleryBlock />
+               <JoinBlock />
             </div>
          }
       />
