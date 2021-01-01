cc1c31a673ff48ad1c2a0573c3d3fc966e814360
LINNI2017 <cai20170607@gmail.com>
2020-12-31 20:36:06 -0800

    add calendar block

---
+++ src/components/CalendarBlock.js
@@ -0,0 +1,51 @@
+import "react-calendar/dist/Calendar.css"
+import React, { useState } from "react"
+import Calendar from "react-calendar"
+import EventBlock from "../components/EventBlock"
+import { Row, Col, Button } from "react-bootstrap"
+import { getDateStr } from "../functions/utils"
+
+const CalendarBlock = () => {
+   const [dateValue, setDateValue] = useState(new Date())
+   const calendarMap = require("../data/calendar")
+   const dateStr = getDateStr(dateValue)
+   const hasEvent = dateStr in calendarMap
+
+   return (
+      <div style={{ marginBottom: "3rem" }}>
+         <h3>
+            Calendar
+         </h3>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur aipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut placera orci nulla pellentesque dignissim enim.   
+         </p>
+         <Row style={{ marginTop: "2rem" }}>
+            <Col sm={6}>
+               <Calendar
+                  className="join-calendar-layout"
+                  onChange={setDateValue}
+                  value={dateValue}
+               />
+            </Col>
+            <Col sm={1} />
+            <Col sm={5}
+               style={{
+                  display: "flex",
+                  justifyContent: hasEvent ? "flex-start" : "center",
+                  flexDirection: "column"
+               }}
+            >
+            {
+               hasEvent ?
+               calendarMap[dateStr].map((event, idx) =>
+                  <EventBlock event={event} key={`event-${idx}`} />)
+               :
+               <h3>Free! No event available.</h3>
+            }
+            </Col>
+         </Row>
+      </div>
+   )
+}
+
+export default CalendarBlock
