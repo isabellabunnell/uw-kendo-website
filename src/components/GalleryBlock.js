cff38043249d6c5f979f1211175a9266db60c696
LINNI2017 <cai20170607@gmail.com>
2021-01-05 20:16:42 -0800

    add selectable gallery

--- src/components/GalleryBlock.js
+++ src/components/GalleryBlock.js
@@ -1,33 +1,48 @@
-import React from "react"
-import { Carousel } from "react-bootstrap"
+import React, { useState } from "react"
+import { Carousel, Dropdown } from "react-bootstrap"
 import { galleryMap } from "../data/info"
 import { IMAGE_PATH } from "../views/App"
 
 const GalleryBlock = () => {
+   const [event, setEvent] = useState(Object.keys(galleryMap)[0])
+
    return (
       <div style={{ marginBottom: "2rem" }}>
-         <h3 style={{ marginBottom: "1rem" }}>
-            Gallery
-         </h3>
+         <div id="gallery-title-layout">
+            <h3 style={{ marginBottom: "1rem", marginRight: "2rem" }}>
+               Gallery
+            </h3>
+            <Dropdown style={{ marginBottom: "2rem" }}>
+               <Dropdown.Toggle variant="success" id="gallery-dropdown">
+                  {event}
+               </Dropdown.Toggle>
+               <Dropdown.Menu id="gallery-drop-menu">
+                  {
+                     Object.keys(galleryMap).map((group, groupIdx) => (
+                        <Dropdown.Item key={`gallery-drop-${groupIdx}`}
+                           onSelect={(_, event) => setEvent(event.target.innerText)}
+                        >{group}</Dropdown.Item>
+                     ))
+                  }
+               </Dropdown.Menu>
+            </Dropdown>
+         </div>
          <Carousel>
             {
-               Object.keys(galleryMap).map((group, groupIdx) => (
-                  galleryMap[group].map((image, imageIdx) => (
-                     <Carousel.Item key={`gallery-group-${groupIdx}-image-${imageIdx}`}>
-                        <img
-                           className="d-block w-100"
-                           src={`${IMAGE_PATH}/gallery/${group}/${image}`}
-                           alt={`${group}/${image}`}
-                           style={{ borderRadius: "5px" }}
-                        />
-                        <Carousel.Caption>
-                           <h3>{group}</h3>
-                        </Carousel.Caption>
-                     </Carousel.Item>
-                  ))
+               galleryMap[event].map((image, imageIdx) => (
+                  <Carousel.Item key={`gallery-image-${imageIdx}`}>
+                     <img
+                        width="100%"
+                        src={`${IMAGE_PATH}/gallery/${event}/${image}`}
+                        alt={`${event}/${image}`}
+                        style={{ borderRadius: "5px" }}
+                     />
+                     <Carousel.Caption>
+                        <h3>{event}</h3>
+                     </Carousel.Caption>
+                  </Carousel.Item>
                ))
             }
-            
          </Carousel>
       </div>
    )
