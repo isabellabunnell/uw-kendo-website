36a35238a4dd6679e3d852d1a8f4ccd0f85d9bc2
LINNI2017 <cai20170607@gmail.com>
2021-01-08 13:37:44 -0800

    remove unnecessary imports

--- src/views/AwardsPage.js
+++ src/views/AwardsPage.js
@@ -1,11 +1,6 @@
 import React from "react"
 import AwardsBlock from "../components/AwardsBlock"
-import HistoryBlock from "../components/HistoryBlock"
-import InfoBlock from "../components/InfoBlock"
-import InstructorsBlock from "../components/InstructorsBlock"
 import PageStructure from "../components/PageStructure"
-import PeopleBlock from "../components/PeopleBlock"
-import { clubInfo } from "../data/info"
 
 const AwardsPage = () => {
    return (
