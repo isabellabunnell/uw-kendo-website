0360128a3e33e39159d3ec6e74350d04f2a7fa32
LINNI2017 <cai20170607@gmail.com>
2021-01-11 13:29:27 -0800

    update about page image

--- src/data/info.js
+++ src/data/info.js
@@ -46,7 +46,7 @@ export const clubInfo = {
    content: `The Kendo Club at University of Washington (UW) is a sports club sponsored by the Department of Recreational Sports Programs. We're open to currently enrolled UW students and faculty/staff members that have purchased a quarterly or annual IMA Membership Card.
    \n
    We are recognized member dojo of the Pacific Northwest Kendo Federation (PNKF) and the All United States Kendo Federation (AUSKF). Members of the club are eligible to become members in good standing of both federations and are entitled to all privileges of the same.`,
-   img: "info/placeholder.jpg"
+   img: "gallery/2016 Tacoma/Bernice 2.jpg"
 }
 
 export const sportInfo = [
