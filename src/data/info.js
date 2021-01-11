bf6cd537eab185aaf8617472c3132a92fd27adf7
LINNI2017 <cai20170607@gmail.com>
2021-01-11 13:57:45 -0800

    add rest warmup

--- src/data/info.js
+++ src/data/info.js
@@ -225,13 +225,14 @@ export const warmUpList = [
       \n
       calf
       \n
-      achillies
+      achilles
       \n
       groin
       \n
       hamstring
       \n
-      other side`
+      other side`,
+      image: "warmup/leg-stretch.jpg"
    },
    {
       abstract: "Arm Circles",
@@ -244,19 +245,23 @@ export const warmUpList = [
    },
    {
       abstract: "Jougeburi x20",
-      detail: "1/4 Suburi"
+      detail: "1/4 Suburi",
+      image: "warmup/the-jougeburi.gif"
    },
    {
       abstract: "Men-uchi x30",
-      detail: "2/4 Suburi"
+      detail: "2/4 Suburi",
+      image: "warmup/the-menuchi.gif"
    },
    {
       abstract: "Sayu-men x20",
-      detail: "3/4 Suburi"
+      detail: "3/4 Suburi",
+      image: "warmup/the-sayumen.gif"
    },
    {
       abstract: "Haya-suburi 5x20",
-      detail: "4/4 Suburi"
+      detail: "4/4 Suburi",
+      image: "warmup/the-hayasuburi.gif"
    }
 ]
 
